a4a1419511b8a079afa7cb493961d289eb2ba30c
Kevin Inz <45952314+KevinInzu@users.noreply.github.com>
2023-04-14 21:01:42 -0700

    Added HeartRate - Prototype

--- Src/Src.ino
+++ Src/Src.ino
@@ -25,7 +25,6 @@
 
 #include <Wire.h>
 #include "MAX30105.h"
-
 #include "heartRate.h"
 
 MAX30105 particleSensor;
@@ -35,13 +34,20 @@ byte rates[RATE_SIZE]; //Array of heart rates
 byte rateSpot = 0;
 long lastBeat = 0; //Time at which the last beat occurred
 
+byte ledBrightness = 0x7f;
+byte sampleAverage = 4;
+byte ledMode = 2;
+int sampleRate = 200;
+int pulseWidth = 411;
+int adcRange = 16384;
+
 float beatsPerMinute;
 int beatAvg;
 
 void setup()
 {
   Serial.begin(115200);
-  Serial.println("Initializing...");
+  Serial.println("Initializing");
 
   // Initialize sensor
   if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
@@ -51,9 +57,9 @@ void setup()
   }
   Serial.println("Place your index finger on the sensor with steady pressure.");
 
-  particleSensor.setup(); //Configure sensor with default settings
-  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
-  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
+  particleSensor.setup(ledBrightness,sampleAverage,ledMode,sampleRate, pulseWidth, adcRange); //Configure sensor with default settings
+  // particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
+  // particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
 }
 
 void loop()
