950b273d103398d31222616610299840e45004f3
Kevin Inz <45952314+KevinInzu@users.noreply.github.com>
2023-04-16 20:31:47 -0700

    HR and SP02 Header Files- Prototype

--- Src/HR/HR.ino
+++ Src/HR/HR.ino
@@ -1,38 +1,9 @@
-/*
-  Optical Heart Rate Detection (PBA Algorithm) using the MAX30105 Breakout
-  By: Nathan Seidle @ SparkFun Electronics
-  Date: October 2nd, 2016
-  https://github.com/sparkfun/MAX30105_Breakout
-
-  This is a demo to show the reading of heart rate or beats per minute (BPM) using
-  a Penpheral Beat Amplitude (PBA) algorithm.
-
-  It is best to attach the sensor to your finger using a rubber band or other tightening
-  device. Humans are generally bad at applying constant pressure to a thing. When you
-  press your finger against the sensor it varies enough to cause the blood in your
-  finger to flow differently which causes the sensor readings to go wonky.
-
-  Hardware Connections (Breakoutboard to Arduino):
-  -5V = 5V (3.3V is allowed)
-  -GND = GND
-  -SDA = A4 (or SDA)
-  -SCL = A5 (or SCL)
-  -INT = Not connected
-
-  The MAX30105 Breakout can handle 5V or 3.3V I2C logic. We recommend powering the board with 5V
-  but it will also run at 3.3V.
-*/
-
 #include <Wire.h>
 #include "MAX30105.h"
 #include "heartRate.h"
+#include "Beat.h"
 
 MAX30105 particleSensor;
-
-const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
-byte rates[RATE_SIZE]; //Array of heart rates
-byte rateSpot = 0;
-long lastBeat = 0; //Time at which the last beat occurred
 
 byte ledBrightness = 0x7f;
 byte sampleAverage = 4;
@@ -41,8 +12,7 @@ int sampleRate = 200;
 int pulseWidth = 411;
 int adcRange = 16384;
 
-float beatsPerMinute;
-int beatAvg;
+int beats;
 
 void setup()
 {
@@ -66,31 +36,9 @@ void loop()
 {
   long irValue = particleSensor.getIR();
 
-  if (checkForBeat(irValue) == true)
-  {
-    //We sensed a beat!
-    long delta = millis() - lastBeat;
-    lastBeat = millis();
 
-    beatsPerMinute = 60 / (delta / 1000.0);
+  beatAvg =getBPM(irValue);
 
-    if (beatsPerMinute < 255 && beatsPerMinute > 20)
-    {
-      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
-      rateSpot %= RATE_SIZE; //Wrap variable
-
-      //Take average of readings
-      beatAvg = 0;
-      for (byte x = 0 ; x < RATE_SIZE ; x++)
-        beatAvg += rates[x];
-      beatAvg /= RATE_SIZE;
-    }
-  }
-
-  Serial.print("IR=");
-  Serial.print(irValue);
-  Serial.print(", BPM=");
-  Serial.print(beatsPerMinute);
   Serial.print(", Avg BPM=");
   Serial.print(beatAvg);
 
--- Src/SPO2/SPO2.ino
+++ Src/SPO2/SPO2.ino
@@ -1,16 +1,10 @@
 #include <Wire.h>
 #include "MAX30105.h" // Sparkfun MAX3010X library
+#include "SP02.h"
+
 MAX30105 particleSensor;
 
-double avered = 0; 
-double aveir = 0;
-double sumirrms = 0;
-double sumredrms = 0;
-int i = 0;
-int Num = 100; // calculate SpO2 by this sampling interval
-float ESpO2; // initial value of estimated SpO2
-double FSpO2 = 0.7; // filter factor for estimated SpO2
-double frate = 0.95; // low pass filter for IR/red LED value to eliminate AC component
+
 #define TIMETOBOOT 3000 // wait for this time(msec) to output SpO2
 #define SCALE 88.0 // adjust to display heart beat and SpO2 in the same scale
 #define SAMPLING 5 // if you want to see heart beat more precisely, set SAMPLING to 1
@@ -18,9 +12,6 @@ double frate = 0.95; // low pass filter for IR/red LED value to eliminate AC com
 
 void setup() {
   Serial.begin(115200);
-
-  // digitalWrite(Greenled, LOW);
-  // digitalWrite(Redled, LOW);
 
   // Initialize sensor
   while (!particleSensor.begin(Wire, I2C_SPEED_FAST)) { // Use default I2C port, 400kHz speed
@@ -38,39 +29,8 @@ void setup() {
   // Set up the wanted parameters
   particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); // Configure sensor with these settings
 
-  particleSensor.enableDIETEMPRDY();
 }
 void loop()
 {
-
-  uint32_t ir, red;
-  double fred, fir;
-  double SpO2 = 0; //raw SpO2 before low pass filtered
-
-  particleSensor.check(); //Check the sensor, read up to 3 samples
-
-  while (particleSensor.available()) {//do we have new data
-
-    red = particleSensor.getFIFOIR(); //why getFOFOIR output Red data by MAX30102 on MH-ET LIVE breakout board
-    ir = particleSensor.getFIFORed(); //why getFIFORed output IR data by MAX30102 on MH-ET LIVE breakout board
-
-    i++;
-    fred = (double)red;
-    fir = (double)ir;
-    avered = avered * frate + (double)red * (1.0 - frate);//average red level by low pass filter
-    aveir = aveir * frate + (double)ir * (1.0 - frate); //average IR level by low pass filter
-    sumredrms += (fred - avered) * (fred - avered); //square sum of alternate component of red level
-    sumirrms += (fir - aveir) * (fir - aveir);//square sum of alternate component of IR level
-
-    if ((i % Num) == 0) {
-      double R = (sqrt(sumredrms) / avered) / (sqrt(sumirrms) / aveir);
-      SpO2 = -23.3 * (R - 0.4) + 100; //http://ww1.microchip.com/downloads/jp/AppNotes/00001525B_JP.pdf
-      ESpO2 = FSpO2 * ESpO2 + (1.0 - FSpO2) * SpO2;//low pass filter
-      sumredrms = 0.0; sumirrms = 0.0; i = 0;
-      break;
-    }
-    Serial.println("SP02");
-    Serial.println(ESpO2);
-    particleSensor.nextSample(); //We're finished with this sample so move
-  }
+  Serial.println(getSPO2(particleSensor));
 }
